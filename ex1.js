12e63b07b9f85a51ce43a5d49f9c85d2d6ab1b78
Kia Fathi <kiafathi@me.com>
2014-11-29 12:45:19 -0800

    (feat) Write some cool curry examples

--- ex1.js
+++ ex1.js
@@ -16,6 +16,31 @@ function split(character, str){
   return str.split(character);
 }
 
-var _words = curry(split)(' ');
+console.log(split('b', 'abc'));
 
-console.log(_words('alabama arkansas china'));
+var _split = curry(split);
+
+var words = _split(' ');
+
+function multiply(a, b){
+  return a * b;
+}
+
+var _multiply = curry(multiply);
+
+var triple = _multiply(3);
+
+function map(callback, list){
+  var newArray = [];
+  for(var i =0; i < list.length; i++){
+    newArray.push(callback(list[i]));
+  }
+  return newArray;
+}
+var tripleList = curry(map)(triple);
+
+console.log(words('alabama arkansas china'));
+
+console.log(triple(9));
+
+console.log(tripleList([1,2,3]));
